6e63b17facb8f6dd5af7800b6f474d9cc1e07eff
Kyle Sauder <ksauder@gmail.com>
2026-06-28 20:21:09 -0400

    update install scripts:

--- install_home.sh
+++ install_home.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-nix run home-manager/master -- switch --flake github:ksauder/nix-home#kyle
+BRANCH="main"
+
+if ! command -v nix >/dev/null 2>&1; then
+    curl -fsSL https://raw.githubusercontent.com/ksauder/nix-home/refs/heads/${BRANCH}/install_nix.sh | bash
+fi
+
+arch="$(uname -m)"
+os="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+nix run home-manager/master -- switch --flake github:ksauder/nix-home/${BRANCH}#kyle@${arch}-${os}
--- install_nix.sh
+++ install_nix.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-sh <(curl -L https://nixos.org/nix/install) --daemon
+ERRSTR=""
+if ! command bash --version >/dev/null 2>&1; then
+    ERRSTR="${ERRSTR}- Install bash\n"
+fi
+
+if ! command curl --version >/dev/null 2>&1; then
+    ERRSTR="${ERRSTR}- Install curl\n"
+fi
+if ! command xz --version >/dev/null 2>&1; then
+    ERRSTR="${ERRSTR}- Install xz\n"
+fi
+
+if [ "$ERRSTR" ]; then
+    echo "ERROR: requirements not met:"
+    echo -e "$ERRSTR"
+    exit 1
+fi
+
+curl -L https://nixos.org/nix/install | sh -s -- --daemon
 mkdir -p ~/.config/nix
 echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
 
